fd85f1529b2a0a63a3bffa2cf6b11795f981699c
Francesco <goras.francesco@gmail.com>
2021-11-25 22:51:32 +1300

    Comments clarifying purpose of each attribute of Vaccine passport

--- eu-dcc.go
+++ eu-dcc.go
@@ -15,28 +15,36 @@ type HCert struct {
 
 type DCCPayload struct {
 	Nam Nam    `json:"nam"`
-	Dob string `json:"dob"`
-	V   []V    `json:"v"`
+	Dob string `json:"dob"` // Date of birth in range of 1900-0-01 to 2099-12-31
+	V   []V    `json:"v"`   // Vaccination group
 	Ver string `json:"ver"`
 }
 
 type Nam struct {
-	Fn  string `json:"fn"`
-	Fnt string `json:"fnt"`
-	Gn  string `json:"gn"`
-	Gnt string `json:"gnt"`
+	Fn  string `json:"fn"`  // Surname
+	Fnt string `json:"fnt"` // Standardised Surname
+	Gn  string `json:"gn"`  // Forename
+	Gnt string `json:"gnt"` // Standardised Forename
 }
 
-// DCC Payload for Vaccination type of HCERT
+// DCC Payload for Vaccination group type of HCERT
 type V struct {
-	Dn int    `json:"dn"`
-	Sd int    `json:"sd"`
-	Mp string `json:"mp"`
-	Dt string `json:"dt"`
-	Tg string `json:"tg"`
-	Vp string `json:"vp"`
-	Ma string `json:"ma"`
-	Co string `json:"co"`
-	Is string `json:"is"`
-	Ci string `json:"ci"`
+	Dn int    `json:"dn"` // Number in a series of doses
+	Sd int    `json:"sd"` // The overall number of doses
+	Mp string `json:"mp"` // Vaccine Product
+	Dt string `json:"dt"` // Date of vaccination
+	Tg string `json:"tg"` // Disease or agent targeted
+	Vp string `json:"vp"` // Vaccine or Prophylaxis
+	Ma string `json:"ma"` // Vaccine marketing authorisation holder or manufacturer
+	Co string `json:"co"` // Member state which administered the vaccine
+	Is string `json:"is"` // Certificate Issuer
+	Ci string `json:"ci"` // Unique certificate identifier
 }
+
+var (
+	GreenPassVersion       = "1.3.0"
+	SNOMEDCovidCode        = "840539006"
+	VaccineProduct         = "EU/1/20/1528"
+	VaccineType            = "1119349007"    // SARS-CoV-2 mRNA vaccine
+	MarketingAuthorisation = "ORG-100030215" // Biontech Manufacturing GmbH
+)
