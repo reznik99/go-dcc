7e5bc0aca8b32361c84a6daef85742f44e554f11
Francesco <goras.francesco@gmail.com>
2021-12-02 00:28:50 +1300

    Improved DCC Parser to return more useful data

--- dcc/dcc-parser.go
+++ dcc/dcc-parser.go
@@ -1,75 +1,67 @@
 package dcc
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"strings"
 
 	"github.com/dasio/base45"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/fxamacker/cbor/v2"
 	"github.com/veraison/go-cose"
 )
 
-func ParseGreenpass(path string, fileType int) (*DCC, error) {
+func ParseGreenpass(path string, fileType int) (payload *DCC, headers *cose.Headers, messageRaw *cose.Sign1Message, signature []byte, err error) {
 
 	if fileType == TypeQRCode {
-		return nil, fmt.Errorf("QRCode parsing not yet implemented")
+		err = fmt.Errorf("QRCode parsing not yet implemented")
+		return
 	}
 
 	// Read vaccine pass
 	fileBytes, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, err
+		return
 	}
 
 	// remove magic header : HC1: (Health Certificate Version 1)
 	dccBase45 := string(fileBytes)
 	if !strings.HasPrefix(dccBase45, "HC1:") {
-		return nil, fmt.Errorf("data does not appear to be EU DCC / Vaccine Passport")
+		err = fmt.Errorf("data does not appear to be EU DCC / Vaccine Passport")
+		return
 	}
 	dccBase45 = strings.Split(dccBase45, "HC1:")[1]
 
 	// Decode base45
 	dccCOSECompressed, err := base45.DecodeString(dccBase45)
 	if err != nil {
-		return nil, err
+		return
 	}
 
 	// Decompress Binary COSE Data with zlib
 	dccCOSE, err := zlibDecompress(dccCOSECompressed)
 	if err != nil {
-		return nil, err
+		return
 	}
 
 	// Unmarshal COSE
 	msg := cose.NewSign1Message()
 	err = msg.UnmarshalCBOR(dccCOSE)
 	if err != nil {
-		return nil, err
+		return
 	}
 
 	// Read CBOR DCC Payload into JSON Struct
 	dcc := &DCC{}
 	err = cbor.Unmarshal(msg.Payload, dcc)
 	if err != nil {
-		return nil, err
+		return
 	}
 
-	// Marshal to JSON to pretty-print
-	jsonDCC, err := json.MarshalIndent(dcc, "", "	")
-	if err != nil {
-		return nil, err
-	}
+	// We got the goods! Return them
+	payload = dcc
+	headers = msg.Headers
+	signature = msg.Signature
+	messageRaw = msg
 
-	// We got the goods!
-
-	fmt.Printf("Headers: %v\n", msg.Headers)
-
-	fmt.Printf("Payload: %s\n", string(jsonDCC))
-
-	fmt.Printf("Signature: %s\n", hexutil.Encode(msg.Signature))
-
-	return nil, nil
+	return
 }
