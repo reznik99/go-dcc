1536415512b911bf23ac7fcd704cc7b89a948a06
Francesco <goras.francesco@gmail.com>
2021-12-01 20:29:55 +1300

    KID fetching logic using Italian API

--- dcc/dcc-validator.go
+++ dcc/dcc-validator.go
@@ -1,6 +1,19 @@
 package dcc
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+var (
+	API_BASE_URL = "https://get.dgc.gov.it/v1/dgc"
+)
+
+type KIDsList struct {
+	KIDs []string
+}
 
 func VerifyGreenpass(filePath string, fileType int) {
 
@@ -12,4 +25,26 @@ func VerifyGreenpass(filePath string, fileType int) {
 	// Validation logic
 	// TODO: Fetch certificates/signers from EU-DCC API and check against KID in Protected Header
 
+	kids := fetchValidKIDs()
+	for _, kid := range kids.KIDs {
+		fmt.Println(kid)
+	}
+}
+
+func fetchValidKIDs() *KIDsList {
+	resp, err := http.Get(API_BASE_URL + "/signercertificate/status")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	kids := new(KIDsList)
+	err = json.Unmarshal(bodyBytes, kids)
+	if err != nil {
+		panic(err)
+	}
+	return kids
 }
