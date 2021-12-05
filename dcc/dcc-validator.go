649cfa845b14752a6073a9dbc7dc107ff06353c7
Francesco <goras.francesco@gmail.com>
2021-12-06 01:02:36 +1300

    Fixing up validation code

--- dcc/dcc-validator.go
+++ dcc/dcc-validator.go
@@ -1,7 +1,7 @@
 package dcc
 
 import (
-	"crypto/ecdsa"
+	"crypto"
 	"crypto/sha256"
 	"crypto/x509"
 	"encoding/base64"
@@ -10,6 +10,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -21,10 +22,10 @@ var (
 )
 
 func VerifyGreenpass(filePath string, fileType int) {
-
+	// Parse certificate from raw or QR code, returning raw cose and parsed dcc payload
 	dcc, raw, err := ParseGreenpass(filePath, fileType)
 	if err != nil {
-		fmt.Printf("%s\n", err.Error())
+		log.Fatalf("%s\n", err.Error())
 	}
 
 	// Fetch KIDs to compare
@@ -36,64 +37,46 @@ func VerifyGreenpass(filePath string, fileType int) {
 	kids := fetchCerts(kidsList)
 
 	// Extract KID from Passports Header
-	tag, err := cose.GetCommonHeaderTag("kid")
+	kidBytes, err := ExtractHeaderBytes(raw, "kid")
 	if err != nil {
-		panic(err)
+		log.Fatalf("%s\n", err.Error())
 	}
-
-	dccKid := raw.Headers.Protected[tag]
-	if _, ok := dccKid.([]byte); !ok {
-		dccKid = raw.Headers.Unprotected[tag]
-		if _, ok = dccKid.([]byte); !ok {
-			panic(errors.New("ERROR: Couldn't extract KID from Vaccine Passport"))
-		}
-	}
-	var dccKidName = base64.StdEncoding.EncodeToString(dccKid.([]byte))
+	var kid = base64.StdEncoding.EncodeToString(kidBytes)
 
 	// Check KID is in trusted list
-	if _, ok := kids[dccKidName]; !ok {
-		fmt.Printf("vaccine Pass KID %s not found on trusted list at: %s\n", dccKidName, API_BASE_URL)
+	if _, ok := kids[kid]; !ok {
+		log.Fatalf("vaccine Pass KID %s not found on trusted list at: %s\n", kid, API_BASE_URL)
 	}
-	fmt.Printf("Vaccine Pass KID '%s' is trusted\n", dccKidName)
+	fmt.Printf("Vaccine Pass KID '%s' is trusted\n", kid)
 
-	// Parse appropriate PEM of KID
-	pemCertificate := kids[dccKidName]
+	// Parse Signer certificate belonging to KID. Extract public key
+	pemCertificate := kids[kid]
 	fmt.Printf("Vaccine Pass Signer Certificate PEM: \n%s\n", pemCertificate)
 	block, _ := pem.Decode([]byte(pemCertificate))
 	cert, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
-		panic(err)
+		log.Fatalf("%s\n", err.Error())
 	}
 
-	// Extract Public Key from Signer Certificate
-	switch cert.PublicKeyAlgorithm {
-	case x509.RSA:
-		fmt.Println("Signer certificate that signed this Vaccine Passport is of type: RSA")
-		return
-	case x509.ECDSA:
-		fmt.Println("Signer certificate that signed this Vaccine Passport is of type: ECDSA")
-	}
-	ecdsaPublic := cert.PublicKey.(*ecdsa.PublicKey)
+	publicKey := cert.PublicKey.(crypto.PublicKey)
 
-	// Extra information for debugging
+	// Print extra information for debugging
 	toBeSigned, _ := raw.SigStructure(nil)
 	digest := sha256.Sum256(toBeSigned)
 	fmt.Printf("Digest: %s\n", hexutil.Encode(digest[:]))
 	fmt.Printf("Signature: %s\n", hexutil.Encode(raw.Signature))
 
-	// Work around to get Algorithm struct
-	// (Curve is not exported and therefore cannot be initialised outside the go-cose library)
+	// Work around to get Algorithm struct (Curve is not exported and therefore cannot be initialised outside the go-cose library)
 	signer, _ := cose.NewSigner(cose.ES256, nil)
-
-	// Verify the Vaccine Passport's Signature
 	verifier := cose.Verifier{
-		PublicKey: ecdsaPublic,
+		PublicKey: publicKey,
 		Alg:       signer.GetAlg(),
 	}
+
+	// Verify the Vaccine Passport's Signature
 	err = raw.Verify(nil, verifier)
 	if err != nil {
-		fmt.Printf("Verification FAILED with err: %s\n", err.Error())
-		return
+		log.Fatalf("Verification FAILED with err: %s\n", err.Error())
 	}
 	fmt.Printf("%s's Vaccine Passport has Signature Validated succesfully\n", dcc.HealthCertificate.DGC.Nam.Gn)
 }
@@ -154,4 +137,22 @@ func fetchValidKIDs() []string {
 	}
 
 	return kids
+}
+
+// ExtractHeaderBytes extracts header bytes with given tag name, from Protected or Unprotected header in cose signature object
+func ExtractHeaderBytes(raw *cose.Sign1Message, headerName string) ([]byte, error) {
+	tag, err := cose.GetCommonHeaderTag(headerName)
+	if err != nil {
+		return nil, err
+	}
+
+	dccKid := raw.Headers.Protected[tag]
+	if _, ok := dccKid.([]byte); !ok {
+		dccKid = raw.Headers.Unprotected[tag]
+		if _, ok = dccKid.([]byte); !ok {
+			return nil, errors.New("ERROR: Couldn't extract KID from Vaccine Passport")
+		}
+	}
+
+	return dccKid.([]byte), nil
 }
